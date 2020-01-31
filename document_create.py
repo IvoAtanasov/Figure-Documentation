136cb89fdafd0e7ec157151abe2a1ca7c5799281
Ivo Ivanov Atanasov <ivoatanasov85@gmail.com>
2020-01-31 14:06:38 +0200

    Add module

--- document_create.py
+++ document_create.py
@@ -1,12 +1,13 @@
 from docx import Document
 from docx.shared import Inches
 
-document = Document()
+def AddpictureToWordFile(picture,file,text_below,text_above):
+    document = Document()
 
-p = document.add_paragraph()
-r = p.add_run()
-r.add_text('Good Morning every body,This is my ')
-r.add_picture('C:\\Users\\Administrator\\Documents\\Python\\Figure Documentation\\Figure-Documentation\\SamplePictures\\ErrorRemedy.PNG')
-r.add_text(' do you like it?')
+    p = document.add_paragraph()
+    r = p.add_run()
+    r.add_text(text_above)
+    r.add_picture(picture)
+    r.add_text(text_below)
 
-document.save('demo.docx')
+    document.save(file)
---
+++ folder_managnment.py
@@ -0,0 +1,3 @@
+import document_create
+
+document_create.AddpictureToWordFile("C:\\Users\\Administrator\\Desktop\\FixLicense.PNG","C:\\test.docx","Text above","Text below")
