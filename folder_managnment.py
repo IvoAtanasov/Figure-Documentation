a824a493298dff3045248d5b1a984a5e1a6fb1fc
Ivo Ivanov Atanasov <ivoatanasov85@gmail.com>
2020-02-02 18:34:53 +0200

    add files

--- folder_managnment.py
+++ folder_managnment.py
@@ -1,3 +1,7 @@
 import document_create
 
-document_create.AddpictureToWordFile("C:\\Users\\Administrator\\Desktop\\FixLicense.PNG","C:\\test.docx","Text above","Text below")
+file="C:\\testv2.docx"
+document_create.AddpictureToWordFile("C:\\Users\\Administrator\\Desktop\\FixLicense.PNG",file,"Text above","Text below")
+document_create.AddpictureToWordFile("C:\\Users\\Administrator\\Desktop\\FixLicense.PNG",file,"Another text","other t")
+
+
