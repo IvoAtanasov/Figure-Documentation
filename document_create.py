d9dbf7e8027b62ad72f5241ff4ee93132ffd07c1
Ivo Ivanov Atanasov <ivoatanasov85@gmail.com>
2020-02-06 16:35:37 +0200

    align screenshot and names

--- document_create.py
+++ document_create.py
@@ -3,20 +3,18 @@ from docx import Document
 from docx.shared import Inches
 import folder_managnment
 
-def AddpictureToWordFile(targetfile,text_below,text_above):
+def AddpictureToWordFile(targetfile):
     document = Document()
     folderPath=folder_managnment.folder
 
-    p = document.add_paragraph()
-    r = p.add_run()
-
+    document.add_paragraph()
+    # r = p.add_run()
     with os.scandir(folderPath) as entries:
         for entry in entries:
             # print(entry.name)
-            r.add_text(text_above)
-            r.add_picture(folderPath+ "\\" + entry.name)
-            r.add_text(text_below)
+            #r.add_text(text_above)
+            document.add_picture(folderPath+ "\\" + entry.name)
+            document.add_paragraph(entry.name)
 
-    
     document.save(targetfile)
     
--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import document_create
 
-file="C:\\testv4.docx"
+file="C:\\testv1.docx"
 #document_create.AddpictureToWordFile("C:\\Users\\Administrator\\Desktop\\FixLicense.PNG",file,"Text above","Text below")
 #document_create.AddpictureToWordFile("C:\\Users\\Administrator\\Desktop\\FixLicense.PNG",file,"Another text","other t")
 
-document_create.AddpictureToWordFile(file,"Text above","Text below")
+document_create.AddpictureToWordFile(file)
